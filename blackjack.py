2803b413da12ecd432a00b44e8bccc6a5ec533f1
borispuljak <boris.puljak123@gmail.com>
2023-08-18 13:54:16 +0200

    Brojanje i novac

--- blackjack.py
+++ blackjack.py
@@ -18,7 +18,7 @@ class Deck:
         self.deck = []
         for suit in suits:
             for rank in ranks:
-                self.deck.append(Card(suit,rank))
+                self.deck.append(Card(suit, rank))
     def __str__(self):
         deck_comp = ""
         for card in self.deck:
@@ -28,4 +28,28 @@ class Deck:
         random.shuffle(self.deck)
     def deal(self):
         single_card = self.deck.pop()
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == "Ace":
+            self.aces += 1
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces > 0:
+            self.value -= 10
+            self.aces -= 1
+
+class Chips:
+    def __init__(self):
+        self.total = 500
+        self.bet = 0
+    def win_bet(self):
+        self.total += self.bet
+    def lose_bet(self):
+        self.total -= self.bet
 
